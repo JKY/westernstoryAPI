0383bffb6584a4908553c0fbcff897a9dbd88f77
fedor.jia <332762488@qq.com>
2015-06-09 16:43:57 +0800

    no message

--- src/main/java/com/westernstory/api/config/Config.java
+++ src/main/java/com/westernstory/api/config/Config.java
@@ -7,6 +7,12 @@ public class Config {
 
     public static final String WEB_KEY = "43869527";
 
+    public static final String FOLDER_UPLOAD = "upload";
+
+    public static final String COOKIE_TICKET_IDENTIFY = "westernstory_ticket_identity";
+
+    public static final String COOKIE_TICKET_VENDOR_PASS = "369821";
+
     public static final String URL_ROOT = "http://120.26.115.144:8080/wsapi/"; // API根路径
 
     public static final String URL_STATIC_NOSLASH = "http://120.26.115.144:808"; // 图片根路径
